7a101d05a29e1c8d5f9ac22ab8dbae086b40cb79
Gabriel Bustamante <gabrielbusta@gmail.com>
2016-11-13 15:15:54 -0600

    added namespaces:

--- object-tracking/main.cpp
+++ object-tracking/main.cpp
@@ -6,56 +6,61 @@
 #include <iostream>
 #include <vector>
 
-std::vector<cv::Rect> detectPedestrians(cv::Mat frame)
+using namespace cv;
+using namespace std;
+
+vector<Rect> detectPedestrians(Mat frame)
 {
     /*
     create a histograms of oriented gradients. Then, set it up to detect
     objects using the trained people detecting support vector machine.
     */
-    cv::HOGDescriptor HOG;
-    HOG.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
-    std::vector<cv::Rect> pedestrians;
+    HOGDescriptor HOG;
+    HOG.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+    vector<Rect> pedestrians;
 
-    HOG.detectMultiScale(frame, pedestrians, 0, cv::Size(4, 4),
-                         cv::Size(8, 8), 1.05);
+//    HOG.detectMultiScale(frame, pedestrians, 0, Size(4, 4),
+//                         cv::Size(32, 32), 1.05, 2);
 
     return pedestrians;
 }
 
-cv::Mat drawRects(cv::Mat frame, std::vector<cv::Rect> pedestrians)
+Mat drawRects(Mat frame, vector<Rect> pedestrians)
 {
-    cv::Mat doctoredFrame = frame.clone();
+    Mat doctoredFrame = frame.clone();
     return doctoredFrame;
 }
 
 int main(int argc, char* argv[], char* envp[])
 {
-    cv::VideoCapture capture("Walk.mpg");
+    VideoCapture capture("Walk.mpg");
     if (!capture.isOpened())
     {
-        std::cout << "ERROR: FAILED TO OPEN THE INPUT FILE." << std::endl;
+        cout << "ERROR: FAILED TO OPEN THE INPUT FILE." << endl;
         return EXIT_FAILURE;
     }
 
-    std::string windowName = "Pedestrian Detection";
+    string windowName = "Pedestrian Detection";
 
     /* create a resizable window. */
-    // cv::namedWindow(windowName, cv::WINDOW_KEEPRATIO);
+    namedWindow(windowName, cv::WINDOW_KEEPRATIO);
 
-    cv::Mat frame;
+    Mat frame;
     while (capture.read(frame))
     {
         if (frame.empty())
         {
             break;
         }
-        std::vector<cv::Rect> pedestrians = detectPedestrians(frame);
-        cv::imshow(windowName, drawRects(frame, pedestrians));
+
+        vector<Rect> pedestrians = detectPedestrians(frame);
+
+        imshow(windowName, drawRects(frame, pedestrians));
         /*
         wait 30 milliseconds before loading and displaying
         the next frame. This displays ~24 frames per second.
         */
-        cv::waitKey(30);
+        waitKey(30);
     }
 
     return EXIT_SUCCESS;
